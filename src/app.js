ee86df2e3c77f1e49f9e74959f9fb6b7bf1bf661
SamuelXing <zixing@syr.edu>
2018-09-15 00:52:29 -0700

    add

--- src/app.js
+++ src/app.js
@@ -1,16 +1,127 @@
-var user = {
-    name: 'Andrew',
-    age: 26,
-    location: 'San Francisco'
-};
+class ContactApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+        this.handleRemoveContact = this.handleRemoveContact.bind(this);
+        this.handleEditContact = this.handleEditContact.bind(this);
+        this.handleAddContact = this.handleAddContact.bind(this);
+        this.state = {
+            contacts: ['One', 'Two', 'Three'] 
+        };
+    }
 
-var template = (
-    <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
-    </div>
-);
+    // remove all 
+    handleRemoveAll() {
+        this.setState(() => {
+            return {
+                contacts: []
+            };
+        });
+    }
 
-var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+    handleAddContact(contact){
+        this.setState((prevState) => {
+            return {
+                contacts: prevState.contacts.concat(contact)
+            }
+        })
+    }
+
+    handleRemoveContact(contactID) {
+        console.log('test remove');
+    }
+
+    handleEditContact(contactID){
+        console.log('test edit');
+    }
+
+    render() {
+        const title = 'Contact Management';
+        const subtitle = 'A single page App to manage your contacts.';
+
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle} />
+                <AddContact
+                    handleAddContact = {this.handleAddContact}
+                />
+                <Contacts 
+                    contacts={this.state.contacts}
+                    handleRemoveAll = {this.handleRemoveAll}
+                    handleEditContact = {this.handleRemoveContact}
+                    handleEditContact = {this.handleEditContact}
+                />
+            </div>
+        )
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
+            </div>
+        );
+    }
+}
+
+class AddContact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddContact =this.handleAddContact.bind(this);
+    }
+    handleAddContact(e){
+        e.preventDefault();
+        
+        // TODO: has to make the data type to be OBJ
+        const contact = e.target.elements.option.value.trim();
+
+        if(contact){
+            this.props.handleAddContact(contact);
+        }
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleAddContact}>
+                    <input type="text" name="option" />
+                    <button>Add Contact</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+class Contacts extends React.Component {
+    render() {
+        return (
+            <div>
+                <button onClick={this.props.handleRemoveAll}>Remove All</button>
+                {
+                    this.props.contacts.map((contact) => <Contact 
+                                                            key={contact} 
+                                                            details={contact} 
+                                                            remove = {this.props.handleRemoveContact} 
+                                                            edit = {this.props.handleEditContact}
+                                                        />)
+                }
+            </div>
+        );
+    }
+}
+
+class Contact extends React.Component {
+    render() {
+        return (
+            <div>
+                contact: {this.props.details}
+                <button onClick = {this.props.handleEditContact}>edit</button>
+                <button onClick = {this.props.handleRemoveContact}>inactivate</button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<ContactApp />, document.getElementById('app'));
